e6a2d3817ea0fca42cb4358e6f67ad5f731e8403
Maxim <yourstenex@gmail.com>
2020-10-26 19:53:56 +0300

    added basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <header className='header'>
+        <div className='container'>
+          <div className='header__wrapper'>
+            <a className='header__logo' href=''>
+              <img src='./img/logo.svg' alt='logo' />
+            </a>
+            <div className='header__search'>
+              <button>
+                <img src='./img/search.svg' alt='' />
+              </button>
+              <input type='text' className='' placeholder='Search movies' />
+            </div>
+          </div>
+        </div>
       </header>
-    </div>
+
+      <div className='content'>
+        <div className='container'>
+          <div className='content__wrapper'>
+            <aside className='navigation'>
+              <ul>
+                <li>
+                  <a
+                    href='#'
+                    className='navigation__link navigation__link--active'
+                  >
+                    New Realeses
+                  </a>
+                </li>
+                <li>
+                  <a href='#' className='navigation__link'>
+                    Best Movies
+                  </a>
+                </li>
+                <li>
+                  <a href='#' className='navigation__link'>
+                    Favorite
+                  </a>
+                </li>
+              </ul>
+            </aside>
+            <main className='main'>
+              <section className='new-releases'>
+                <h2 className='new-releases__title'>New Releases</h2>
+              </section>
+            </main>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
